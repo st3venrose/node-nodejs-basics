5a248f104b2b1d04585215db50ef0794dc529c75
Istvan Rozsa <rozsa.istvan0701@gmail.com>
2024-01-30 00:55:25 +0100

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
+import { spawn } from "child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const script = path.normalize("src/cp/files/script.js");
+  const child = spawn(process.execPath, [script, ...args], {
+    stdio: ["inherit", "inherit", "inherit", "ipc"],
+  });
+
+  child.on("error", (error) =>
+    console.error(`Child process error: ${error.message}`)
+  );
+  child.on("exit", (code) =>
+    console.log(`Child process exited with code ${code}`)
+  );
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2"]);
