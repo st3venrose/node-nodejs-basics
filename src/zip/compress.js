eb071e0c0b54ac57beb9df7cab52439e68d86d72
Istvan Rozsa <rozsa.istvan0701@gmail.com>
2024-01-30 00:18:44 +0100

    File operations

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import { createGzip } from "zlib";
+import { finished } from "stream/promises";
+import path from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(
+    path.normalize("./src/zip/files/fileToCompress.txt")
+  );
+  const writeStream = fs.createWriteStream(
+    path.normalize("./src/zip/files/archive.gz")
+  );
+
+  const gzipStream = createGzip();
+
+  readStream.pipe(gzipStream).pipe(writeStream);
+
+  await finished(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import { createGunzip } from "zlib";
+import { finished } from "stream/promises";
+import path from "path";
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(
+    path.normalize("./src/zip/files/archive.gz")
+  );
+  const writeStream = fs.createWriteStream(
+    path.normalize("./src/zip/files/fileToCompress.txt")
+  );
+
+  const gunzipStream = createGunzip();
+
+  readStream.pipe(gunzipStream).pipe(writeStream);
+
+  await finished(writeStream);
 };
 
 await decompress();
